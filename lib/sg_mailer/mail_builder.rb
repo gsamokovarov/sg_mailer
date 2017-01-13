a5249e63b1d265a8d00c8e7892041d145534b4e3
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-13 20:15:43 +0200

    Let MailBuilder be self-sustained object
    This drops a lot of sendgrid-ruby defined constants. It also decouples
    the object, so now it relies only on itself to get the job done.

--- lib/sg_mailer/mail_builder.rb
+++ lib/sg_mailer/mail_builder.rb
@@ -1,65 +1,37 @@
 module SGMailer
   class MailBuilder
-    def initialize(**kw)
-      @from = kw.fetch(:from)
-      @to = Array(kw.fetch(:to)).uniq
-      @substitutions = Hash(kw.fetch(:substitutions, {}))
-      @template_id = kw[:template_id]
-      @content = kw[:content]
-
-      @mail = Mail.new
-      @personalization = SendGrid::Personalization.new
+    def initialize(from:, to: :template_id, substitutions: {})
+      @from = normalize_email(from)
+      @to = Array(to).map { |email| normalize_email(email) },
+      @substitutions = normalize_substitutions(substitutions)
+      @template_id = template_id
     end
 
     def build
-      add_subject
-      add_templates
-      add_contents
-      add_to
-      add_from
-      add_substitutions
-
-      finalize
-      @mail
+      {
+        from: normalize_email(@from)
+        personalizations: [{ to: @to, substitutions: @substitutions }],
+        content: content_for_template,
+        template_id: @template_id
+      }
     end
 
     private
 
-    def add_subject
-      @mail.subject = @subject
-    end
-
-    def add_templates
-      @mail.template_id = @template_id if @template_id
-    end
-
-    def add_contents
-      @mail.contents = SendGrid::Content.new(type: 'text/html', value: "<html><body>#{@content}</body></html>")
-      @mail.contents = SendGrid::Content.new(type: 'text/plain', value: @content) if @content
-    end
-
-    def add_to
-      @to.each do |address|
-        @personalization.to = new_email(address)
+    def normalize_email(email)
+      if email.is_a?(String)
+        { email: email, name: email.split('@').first.to_s }
+      else
+        email
       end
     end
 
-    def add_from
-      @mail.from = new_email(@from)
+    def normalize_substitutions(substitutions)
+      Hash(substitutions.map { |key, value| [key.to_s, value.to_s] })
     end
 
-    def add_substitutions
-      @substitutions.each do |key, value|
-        @personalization.substitutions = SendGrid::Substitution.new(key: key, value: value.to_s)
-      end
-    end
-
-    def finalize
-      @mail.personalizations = @personalization
-    end
-
-    def new_email(email)
-      SendGrid::Email.new(String === email ? { email: email } : email)
+    def content_for_template
+      [{ type: "text/html", value: "<html><body></body></html>" }]
     end
   end
 end
