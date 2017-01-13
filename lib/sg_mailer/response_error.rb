6910b32cd7c1d73fbe0bf4dfed58919a35338335
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-13 20:05:19 +0200

    Forward ResponseError to Net::HTTPResponse

--- lib/sg_mailer/response_error.rb
+++ lib/sg_mailer/response_error.rb
@@ -3,29 +3,17 @@ module SGMailer
     def initialize(response)
       @response = response
 
-      super(extract_message)
+      super(build_message)
     end
 
-    def status_code
-      @response.status_code
-    end
-
-    def body
-      @body ||= Hash(JSON.parse(@response.body))
-    end
-
-    def headers
-      # Cause somebody decided to inspect a hash and return the string
-      # representation.
-      @headers ||= JSON.parse(@response.headers.gsub('"=>"', '":'))
+    def status
+      @response.status
     end
 
     private
 
-    def extract_message
-      body.fetch('errors', []).reduce('') do |final, err|
-        final << err['message'].to_s
-      end
+    def build_message
+      "Error from SendGrid:\n#{@response.body}"
     end
   end
 end
