272ca0c35025956ed09675e8622ddd6f4067d501
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-15 18:47:26 +0200

    Drop the fully qualified const refs

--- lib/sg_mailer/message_delivery.rb
+++ lib/sg_mailer/message_delivery.rb
@@ -3,11 +3,11 @@ require 'delegate'
 module SGMailer
   class MessageDelivery < SimpleDelegator
     def deliver_now(**options)
-      SGMailer::Job.set(options).perform_now(as_json) if as_json
+      Jb.set(options).perform_now(as_json) if as_json
     end
 
     def deliver_later(**options)
-      SGMailer::Job.set(options).perform_later(as_json) if as_json
+      Job.set(options).perform_later(as_json) if as_json
     end
   end
 end
