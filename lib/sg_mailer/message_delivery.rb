d6fc4d4e45794c029fc6580c8fe698ae2d6810b9
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-15 21:46:18 +0200

    Optionalize the Active Job dependency

--- lib/sg_mailer/message_delivery.rb
+++ lib/sg_mailer/message_delivery.rb
@@ -2,12 +2,24 @@ require 'delegate'
 
 module SGMailer
   class MessageDelivery < SimpleDelegator
-    def deliver_now(**options)
-      Jb.set(options).perform_now(as_json) if as_json
-    end
+    if defined?(ActiveJob)
+      require 'sg_mailer/job'
 
-    def deliver_later(**options)
-      Job.set(options).perform_later(as_json) if as_json
+      def deliver_now(**options)
+        Job.set(options).perform_now(as_json) if as_json
+      end
+
+      def deliver_later(**options)
+        Job.set(options).perform_later(as_json) if as_json
+      end
+    else
+      def deliver_now(**options)
+        SGMailer.send(as_json) if as_json
+      end
+
+      def deliver_later(**options)
+        SGMailer.send(as_json_) if as_json
+      end
     end
   end
 end
