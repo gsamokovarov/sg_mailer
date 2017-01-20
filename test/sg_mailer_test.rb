71142559c0881719500c84821df4674f08e26faa
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-20 17:53:40 +0200

    Test out the testing client creation

--- test/sg_mailer_test.rb
+++ test/sg_mailer_test.rb
@@ -8,4 +8,10 @@ class SGMailerTest < Minitest::Test
 
     assert_equal api_key, SGMailer.client.api_key
   end
+
+  def test_configuration_for_testing_client
+    SGMailer.configure(test_client: true)
+
+    assert SGMailer.client.is_a?(SGMailer::TestClient)
+  end
 end
