28843b4d432d34517266b488abe8bf30ff4ba127
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-20 23:08:00 +0100

    Setup a debugger for the testing

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in sg_mailer.gemspec
 gemspec
+
+gem 'byebug'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+begin
+  require 'byebug'
+rescue LoadError
+  # It's fine.
+end
+
 require 'sg_mailer'
 
 require 'minitest/autorun'
