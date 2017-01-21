05cad24c199a5d5d34e132da7d88700f10acf0c1
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-21 17:12:55 +0100

    Test-out the test client

--- lib/sg_mailer/test_client.rb
+++ lib/sg_mailer/test_client.rb
@@ -2,6 +2,10 @@ module SGMailer
   class TestClient
     attr_accessor :mail_requests
 
+    def initialize
+      @mail_requests = []
+    end
+
     def send(mail)
       mail_requests << mail.to_json
     end
---
+++ test/sg_mailer/test_client_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+module SGMailer
+  class TestClientTest < Minitest::Test
+    def test_empty_mail_request_queue
+      test_client = TestClient.new
+
+      assert_equal [], test_client.mail_requests
+    end
+
+    def test_mail_request_queue
+      test_client = TestClient.new
+
+      template_id = '00000000-0000-0000-0000-000000000000'
+      mail = MailBuilder.new(from: 'gsamokovarov+from@gmail.com',
+                             to: 'gsamokovarov+to@gmail.com',
+                             template_id: template_id)
+
+      test_client.send(mail.build)
+
+      assert_equal test_client.mail_requests.map { |m| JSON.parse(m) },
+        [
+          'from' => { 'email' => 'gsamokovarov+from@gmail.com' },
+          'personalizations' => [
+            'to' => ['email' => "gsamokovarov+to@gmail.com"],
+            'substitutions' => {}
+          ],
+          'content' => [
+            'type' => 'text/html',
+            'value' => '<html><body></body></html>'
+          ],
+          'template_id' => '00000000-0000-0000-0000-000000000000'
+        ]
+    end
+  end
+end
