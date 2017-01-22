3fb6d1e72506fb9223d86e09f01234eb84d1632e
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-22 14:06:12 +0100

    Introduce delivery processor configuration

--- lib/sg_mailer.rb
+++ lib/sg_mailer.rb
@@ -3,22 +3,33 @@ require 'sg_mailer/response_error'
 require 'sg_mailer/mail_builder'
 require 'sg_mailer/client'
 require 'sg_mailer/test_client'
-require 'sg_mailer/message_delivery'
 require 'sg_mailer/base'
 
 module SGMailer extend self
   attr_accessor :client
   attr_accessor :delivery_processor
 
-  def configure(delivery_processor: MessageDelivery,
+  def configure(delivery_processor: default_delivery_processor,
                 test_client: false, **client_options)
     self.client = test_client ? TestClient.new : Client.new(**options)
-    self.delivery_processor = delivery_processor || MessageDelivery
+    self.delivery_processor = delivery_processor
   end
 
   def send(mail)
     raise ConfigurationError if client.nil?
 
     client.send(mail)
+  end
+
+  private
+
+  def default_delivery_processor
+    if defined?(::ActiveJob)
+      require 'sg_mailer/delayed_delivery_processor'
+      DelayedDeliveryProcessor
+    else
+      require 'sg_mailer/immediate_delivery_processor'
+      ImmediateDeliveryProcessor
+    end
   end
 end
