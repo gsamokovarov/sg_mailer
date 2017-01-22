6a948b3bcb28d6cff52b49afbbbd3fe30b5a952d
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-22 15:22:22 +0100

    Make sure the decorator is in the "right" place

--- test/sg_mailer/base_test.rb
+++ test/sg_mailer/base_test.rb
@@ -4,7 +4,6 @@ module SGMailer
   class BaseTest < Minitest::Test
     class SendGridMailer < SGMailer::Base
       template_id 'e0d26988-d1d7-41ad-b1eb-4c4b37125893'
-
       def welcome_mail
         mail from: 'me@foo.com', to: 'ms@foo.com'
       end
