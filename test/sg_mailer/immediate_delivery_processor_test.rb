1e24f1640f4c4657281c8431b2c9d24cd807f786
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-22 22:04:46 +0100

    Introduce smoke tests for the immediate processor

---
+++ test/sg_mailer/immediate_delivery_processor_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+require 'sg_mailer/immediate_delivery_processor'
+
+module SGMailer
+  class ImmediateDeliveryProcessorTest < Minitest::Test
+    def setup
+      SGMailer.configure(api_key: ENV['SG_API_KEY'])
+    end
+
+    def test_no_delayed_delivery
+      template_id = 'e0d26988-d1d7-41ad-b1eb-4c4b37125893'
+      mail = MailBuilder.new(from: 'gsamokovarov+from@gmail.com',
+                             to: 'gsamokovarov+to@gmail.com',
+                             template_id: template_id)
+
+      delivery_processor = ImmediateDeliveryProcessor.new(mail.build)
+      delivery_processor.deliver_now
+    end
+
+    def test_no_delayed_delivery_even_on_deliver_later
+      template_id = 'e0d26988-d1d7-41ad-b1eb-4c4b37125893'
+      mail = MailBuilder.new(from: 'gsamokovarov+from@gmail.com',
+                             to: 'gsamokovarov+to@gmail.com',
+                             template_id: template_id)
+
+      delivery_processor = ImmediateDeliveryProcessor.new(mail.build)
+      delivery_processor.deliver_later
+    end
+  end
+end
