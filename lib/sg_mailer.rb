4b666e7294479546b1560b475b0fb09f51c3b248
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-22 12:32:11 +0100

    Add an option to configure the delivery processor

--- lib/sg_mailer.rb
+++ lib/sg_mailer.rb
@@ -8,14 +8,12 @@ require 'sg_mailer/base'
 
 module SGMailer extend self
   attr_accessor :client
+  attr_accessor :delivery_processor
 
-  def configure(test_client: false, **options)
-    self.client =
-      if test_client
-        TestClient.new
-      else
-        Client.new(**options)
-      end
+  def configure(delivery_processor: MessageDelivery,
+                test_client: false, **client_options)
+    self.client = test_client ? TestClient.new : Client.new(**options)
+    self.delivery_processor = delivery_processor || MessageDelivery
   end
 
   def send(mail)
